ab68cc553395f66ba42509131d3cc56c3ebeb5d8
Abner Lima <abnerlima.1997@gmail.com>
2022-11-30 21:15:18 -0300

    Initial Commit

---
+++ Conversores/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Abner Lima on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    
+    @IBOutlet weak var btUnit1: UIButton!
+    
+    @IBOutlet weak var btUnit2: UIButton!
+    
+    @IBOutlet weak var labelResult: UILabel!
+    
+    @IBOutlet weak var labelResultUnit: UILabel!
+    
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
